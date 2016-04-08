8e34c58d082ba1130ecb66971746acc97afd5942
waad-DB <jaradat.1080414@gmail.com>
2016-04-08 00:14:31 -0700

    Add Skeleton website start page and bootsrap with responsive layout libraries

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/start');
+  response.render('pages/skeleton');
 });
 
 app.listen(app.get('port'), function() {
