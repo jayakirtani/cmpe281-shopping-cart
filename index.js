fcce8f4a97004cd021f16d0d768dcfe95f713be7
Syed Imran Ahmed <syedimran.ahmed@sjsu.edu>
2016-04-09 00:17:19 -0700

    Added the mongo client variable to use the mongo connection.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+/*Added:Imran*/
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
