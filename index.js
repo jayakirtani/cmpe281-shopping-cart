40c6abd4b44b360aa002b8b9e2ef34c62f7041be
Syed Imran Ahmed <syedimran.ahmed@sjsu.edu>
2016-04-09 20:38:57 -0700

    Added the check for mongo-db connection using the connection string

--- index.js
+++ index.js
@@ -21,4 +21,12 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+/*Added: Imran: Testing the mongodb connection hosted on the aws*/
+//var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://imran:imran@ec2-54-173-134-32.compute-1.amazonaws.com:27017/test';  //change the url with the latest one
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
 
