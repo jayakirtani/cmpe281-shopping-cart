428fb4fb0f24ce301332d0ef0996c335b44b7395
waad-DB <jaradat.1080414@gmail.com>
2016-04-11 21:28:50 -0700

    sign up

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var signup = require(__dirname +'/routes/signup');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname+'/routes'));
+
+
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -12,6 +15,11 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/skeleton');
 });
+
+app.get('/signup',signup.getSignup);
+app.post('/signup', signup.createUser);
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
