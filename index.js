6a4a8a1080cc273c3378d23551f9710ee75f44ce
waad-DB <jaradat.1080414@gmail.com>
2016-04-13 14:03:32 -0700

    Signup post request angularjs implementation

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var signup = require(__dirname +'/backend/routes/signup');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,9 +12,7 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/skeleton',{error:false});
 });
-app.get('/signup', signup.getSignup);
-	
-app.post('/signup', signup.createUser);
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
