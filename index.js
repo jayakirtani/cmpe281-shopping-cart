7cbf5fd25af56c3568bb6c641c89ec0c003ef188
waad-DB <jaradat.1080414@gmail.com>
2016-04-12 22:23:35 -0700

    add the front end and back-end calls for Sing-Up

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 var express = require('express');
-var signup = require(__dirname +'/routes/signup');
+var signup = require(__dirname +'/backend/routes/signup');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/public'));
-//app.use(express.static(__dirname+'/routes'));
-
-
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/skeleton');
+  response.render('pages/skeleton',{error:false});
 });
-
-app.get('/signup',signup.getSignup);
+app.get('/signup', signup.getSignup);
+	
 app.post('/signup', signup.createUser);
-
-
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
