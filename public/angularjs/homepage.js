b4b580e5ddadfdd623a5636c761f8e408a7dd4e2
waad-DB <jaradat.1080414@gmail.com>
2016-04-16 22:11:57 -0700

    encryption of password in fronend using CryptoJS module

--- public/angularjs/homepage.js
+++ public/angularjs/homepage.js
@@ -1,18 +1,43 @@
 
+
 var app = angular.module("homePage",[]);
-app.controller('homePagectr',function($scope, $http){
+app.controller('homePagectr',function($scope, $http,$window){
 	$scope.login = function() {
-		$http.get('/signin').success(function() {
-		console.log("go to sigin");
-		}); 
+		console.log($scope.firstName);
+		
+		var hashpassword = md5.createHash($scope.password || '');
+			$http({
+				url:'/login',
+				method:'POST',
+				data: {"email":$scope.email, "password":hashpassword},
+				responseType: "json",
+				headers: {'Content-Type': 'application/json'}
+			}).success(function(data){
+
+		}).error(function(){
+			
+		});
+	};
+	
+	$scope.loginPage = function() {
+		$window.location.href = '/login';
+	};
+	
+	$scope.signupPage= function() {
+		$window.location.href = '/';
 	};
 	
 	$scope.signup = function() {
-		console.log($scope.firstName);
+	//	console.log($scope.firstName); key should be agreed with backend
+		var key = "euhe68vjdr1aX4F091c7aCggSMBf0A7M";
+		 var hashpassword = CryptoJS.AES.decrypt(key, $scope.password);
+		//var  hashpassword = CryptoJS.enc.Utf8.stringify(decrypted);
+		
+		console.log("hashpassword", hashpassword);
 			$http({
 				url:'/signup',
 				method:'POST',
-				data: {"firstName":$scope.firstName, "lastName":$scope.lastName,"email":$scope.email, "password":$scope.password},
+				data: {"firstName":$scope.firstName, "lastName":$scope.lastName,"email":$scope.email, "password":hashpassword},
 				responseType: "json",
 				headers: {'Content-Type': 'application/json'}
 			}).success(function(data){
