0482b2b615eac2431d20fc84d21ce7e8aa8fac65
waad-DB <jaradat.1080414@gmail.com>
2016-04-16 22:15:38 -0700

    add login and signup pages http get implementation

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get('/', function(request, response) {
   response.render('pages/skeleton',{error:false});
 });
 
+app.get('/signup', function(request, response) {
+	  response.render('pages/skeleton',{error:false});
+	});
+
+app.get('/login', function(request, response) {
+	  response.render('pages/login',{error:false});
+	});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
