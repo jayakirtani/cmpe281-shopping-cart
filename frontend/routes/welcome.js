a39635c787fb365e6036f8b8808aae7576747120
waad-DB <jaradat.1080414@gmail.com>
2016-04-22 14:18:24 -0700

    signup post parameters and rest call to the backend api for signup

--- frontend/routes/welcome.js
+++ frontend/routes/welcome.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var querystring = require('querystring');
+var http = require('http');
 var router = express.Router();
 
 /* Render Login page on welcome*/
@@ -26,17 +28,69 @@ router.route('/login')
 	});
 
 router.route('/').get(function(req, res, next) {
-	res.render('welcomePage');
+	res.render('welcomePage',{error:false});
 })
 
 
 
 router.route('/signup').post(function(req, res, next){
 	//read email and password from req object
-	var email = req.params.firstName1;
-	var password = req.params.password;
-	console.log("email :" +email);
-	console.log(req.params);
+	
+	// make a restcall 
+
+	
+	 var post_data = JSON.stringify({
+	      'email' : req.body.email,
+	      'password': req.body.password,
+	      'firstname': req.body.firstname,
+	      'lastname': req.body.lastname,
+	       'address' :{
+	    	 'addrline1':req.body.addressLine1, 
+	    	 'addrline2': req.body.addressLine2,
+	    	  'city' :req.body.city, 
+	    	  'state': req.body.region,
+	    	  'zip': req.body.postalCode,
+	    	  'country': req.body.country,
+	   },
+	    	 
+	    	  'contactnumber': req.body.contactnumber
+	  });
+	 console.log('here they are ', post_data);
+
+	  // add url and host information
+	  var post_options = {
+	      host: 'host',
+	      port: '8080',
+	      path: '/',
+	      method: 'POST',
+	      headers: {
+	          'Content-Type': 'application/x-www-form-urlencoded',
+	          'Content-Length': Buffer.byteLength(post_data)
+	      }
+	  };
+
+	  // Set up the request
+	  var post_req = http.request(post_options, function(res) {
+	      res.setEncoding('utf8');
+	      res.on('data', function (chunk) {
+	          console.log('Response: ' + chunk);
+	          res.render('welcomePage',{signup:true});
+	      
+	          // TODO: read response data :true or false and redirect accordingly 
+	      });
+	      
+	  });
+	  post_req.on('error', function (err) {
+		  console.log("\n on error here \n");
+    	  res.render('welcomePage',{signup:true}); 
+      });
+	  // post the data
+	  post_req.write(post_data);
+	  post_req.end();
+	  
+	  
+			
+			
 })
 
 module.exports = router;
