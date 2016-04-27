5b578e67e9917a2c296803442a73799ba0aa6f9e
kavish jain <kavishme@gmail.com>
2016-04-27 13:05:12 -0700

    added cart code, updated gitignore, updated journal

---
+++ frontend/public/includes/js/cart.js
@@ -0,0 +1,6 @@
+function removeItemFromCart(itemID)
+{
+    //todo: send delete request to /cart with itemID
+    //todo: check if cookie - session info is also sent with ajax call
+    //todo: on success remove tr for corresponding itemID from table of cart items
+}
